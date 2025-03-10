f0dee1010348f549dab1b9f9a860df0fb8ce4fe7
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-03-10 14:39:11 +0100

    Edited tests/ Added more tests for run_a_command() function Changed tests order for better readability Removed some unnecessary code

--- tests/src/tests/run_a_command.test.c
+++ tests/src/tests/run_a_command.test.c
@@ -11,8 +11,10 @@ int	test7(void);
 int	test8(void);
 int	test9(void);
 int	test10(void);
+int test11(void);
+int test12(void);
 
-int	main(void)
+int main(void)
 {
 	int (*tests[])(void) =
 		{
@@ -25,7 +27,9 @@ int	main(void)
 			test7,
 			test8,
 			test9, 
-			test10
+			test10,
+			test11,
+			test12
 		};
 
 	for (int i = 0; i < (int)(sizeof(tests) / sizeof(tests[0])); i++)
@@ -73,9 +77,6 @@ int	test3(void)
 
 	result = run_a_command(command);
 
-	if (result == BINARY_LAUNCH_ERR)
-		result = SUCCESS;
-
 	destroy_command(&command);
 	return (result);
 }
@@ -90,9 +91,6 @@ int	test4(void)
 	command = create_command(exe_path, args, NULL, NULL);
 
 	result = run_a_command(command);
-
-	if (result == BINARY_LAUNCH_ERR)
-		result = SUCCESS;
 
 	destroy_command(&command);
 	return (result);
@@ -109,47 +107,11 @@ int	test5(void)
 
 	result = run_a_command(command);
 
-	if (result < 10)
-		result = SUCCESS;
-
 	destroy_command(&command);
 	return (result);
 }
 
 int	test6(void)
-{
-	int result;
-	t_command *command;
-
-	char *exe_path = "/bin/false";
-	char *args[] = {exe_path, NULL};
-	command = create_command(exe_path, args, NULL, NULL);
-
-	result = run_a_command(command);
-
-	if (result < 10)
-		result = SUCCESS;
-
-	destroy_command(&command);
-	return (result);
-}
-
-int	test7(void)
-{
-	int result;
-	t_command *command;
-
-	char *exe_path = "/bin/true";
-	char *args[] = {exe_path, NULL};
-	command = create_command(exe_path, args, NULL, NULL);
-
-	result = run_a_command(command);
-
-	destroy_command(&command);
-	return (result);
-}
-
-int	test8(void)
 {
 	int result;
 	t_command *command;
@@ -164,22 +126,7 @@ int	test8(void)
 	return (result);
 }
 
-int	test9(void)
-{
-	int result;
-	t_command *command;
-
-	char *exe_path = "/bin/printf";
-	char *args[] = {exe_path, "test", NULL};
-	command = create_command(exe_path, args, NULL, "assets/test9/output.txt");
-
-	result = run_a_command(command);
-
-	destroy_command(&command);
-	return (result);
-}
-
-int	test10(void)
+int test7(void)
 {
 	int result;
 	t_command *command;
@@ -187,6 +134,81 @@ int	test10(void)
 	char *exe_path = "/bin/wc";
 	char *args[] = {exe_path, "-l", NULL};
 	command = create_command(exe_path, args, "assets/test10/input.txt", NULL);
+
+	result = run_a_command(command);
+
+	destroy_command(&command);
+	return (result);
+}
+
+int test8(void)
+{
+	int result;
+	t_command *command;
+
+	char *exe_path = "cowsay";
+	char *args[] = {exe_path, "hello world!", NULL};
+	command = create_command(exe_path, args, NULL, NULL);
+
+	result = run_a_command(command);
+
+	destroy_command(&command);
+	return (result);
+}
+
+int test9(void)
+{
+	int result;
+	t_command *command;
+
+	char *exe_path = "grepo";
+	char *args[] = {exe_path, "h", NULL};
+	command = create_command(exe_path, args, "assets/test11/input.txt", "assets/test11/output.txt");
+
+	result = run_a_command(command);
+
+	destroy_command(&command);
+	return (result);
+}
+
+int test10(void)
+{
+	int result;
+	t_command *command;
+
+	char *exe_path = "/bin/false";
+	char *args[] = {exe_path, NULL};
+	command = create_command(exe_path, args, NULL, NULL);
+
+	result = run_a_command(command);
+
+	destroy_command(&command);
+	return (result);
+}
+
+int test11(void)
+{
+	int result;
+	t_command *command;
+
+	char *exe_path = "/bin/true";
+	char *args[] = {exe_path, NULL};
+	command = create_command(exe_path, args, NULL, NULL);
+
+	result = run_a_command(command);
+
+	destroy_command(&command);
+	return (result);
+}
+
+int	test12(void)
+{
+	int result;
+	t_command *command;
+
+	char *exe_path = "/bin/printf";
+	char *args[] = {exe_path, "test", NULL};
+	command = create_command(exe_path, args, NULL, "assets/test9/output.txt");
 
 	result = run_a_command(command);
 
