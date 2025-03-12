c56593521e8b599027a9e94121a44a981b84cf66
akovtune <akovtune@student.codam.nl>
2025-03-12 14:36:33 +0100

    Created echo builtin

--- src/logic/builtins/echo.c
+++ src/logic/builtins/echo.c
@@ -6,24 +6,54 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 12:57:24 by akovtune          #+#    #+#             */
-/*   Updated: 2025/03/09 17:15:16 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/03/12 14:25:46 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "echo.h"
 
+bool	check_for_n_flag(t_string first_arg);
+
 int	echo(t_string_array args)
 {
-	int	i;
+	bool	n_flag_active;
+	int		i;
 
-	i = -1;
-	while (args[++i])
+	if (!args || !args[1])
+		return (FAILURE);
+	n_flag_active = check_for_n_flag(args[1]);
+	if (!n_flag_active)
+		i = 1;
+	else
+		i = 2;
+	while (args[i])
 	{
 		printf("%s", args[i]);
 		if (args[i + 1])
 			printf(" ");
+		i++;
 	}
-	printf("\n");
-	destroy_string_array(&args);
+	if (!n_flag_active)
+		printf("\n");
 	return (SUCCESS);
 }
+
+bool	check_for_n_flag(t_string first_arg)
+{
+	int	i;
+
+	if (!first_arg || first_arg[0] != '-')
+		return (false);
+	i = 1;
+	while (first_arg[i])
+	{
+		if (first_arg[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+//I need to know whether the first argument is the -n flag, and if it is, 
+//then I don't need to print it (the first argument)
+//and I don't need to print the new line in the end
