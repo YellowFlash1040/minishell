3deb00179d9b075983a8c1d97a8418e91cf748ec
akovtune <akovtune@student.codam.nl>
2025-03-16 15:39:12 +0100

    Made refactoring for "variable" class

--- src/model/variable.h
+++ src/model/variable.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 18:29:18 by akovtune          #+#    #+#             */
-/*   Updated: 2025/03/11 19:46:30 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/03/15 16:54:36 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,9 @@ typedef struct variable
 	t_string	name;
 	t_string	value;
 	bool		is_exported;
-	t_string	string_value;
 }	t_variable;
 
-t_variable	*init_variable(t_string name);
+t_variable	*init_variable(t_string name, t_string value);
 void		destroy_variable(t_variable **variable);
-t_string	convert_variable_to_string(t_variable *variable);
 
 #endif
