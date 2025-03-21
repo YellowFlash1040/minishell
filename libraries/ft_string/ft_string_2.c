472c8f1a6b629470bb9d48752a87fe3152b72f8b
akovtune <akovtune@student.codam.nl>
2025-03-21 15:57:23 +0100

    Added ft_strtrim() and is_whitespace() functions to the ft_string library

--- libraries/ft_string/ft_string_2.c
+++ libraries/ft_string/ft_string_2.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_string_2.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/09 16:35:08 by akovtune          #+#    #+#             */
-/*   Updated: 2025/03/09 17:04:41 by akovtune         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   ft_string_2.c									  :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: akovtune <akovtune@student.42.fr>		  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2025/01/09 16:35:08 by akovtune		  #+#	#+#			 */
+/*   Updated: 2025/03/21 12:42:54 by akovtune		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "ft_string.h"
@@ -44,4 +44,38 @@ char	*ft_strjoin(char *s1, char *s2)
 		res[index++] = s2[i];
 	res[index] = '\0';
 	return (res);
+}
+
+char	*ft_strtrim(char *str)
+{
+	size_t	start;
+	size_t	end;
+	char	*trimmed;
+	size_t	length;
+	size_t	i;
+
+	if (!str)
+		return (NULL);
+	start = 0;
+	while (str[start] && is_whitespace(str[start]))
+		start++;
+	end = start;
+	while (str[end])
+		end++;
+	while (end > start && is_whitespace(str[end - 1]))
+		end--;
+	length = end - start;
+	trimmed = (char *)malloc(length + 1);
+	if (!trimmed)
+		return (NULL);
+	i = -1;
+	while (++i < length)
+		trimmed[i] = str[start + i];
+	trimmed[i] = '\0';
+	return (trimmed);
+}
+
+int	is_whitespace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
 }
