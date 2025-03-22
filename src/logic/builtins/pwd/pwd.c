7c1532b8308ab840acb5f32e55d69350d2d4ebbf
akovtune <akovtune@student.codam.nl>
2025-03-22 18:56:10 +0100

    Made "cd" and "pwd" commands more safe. Now they can handle case, where user unsets "PWD" and/or "OLDPWD" variable(s) using the "unset" command before calling "pwd" or "cd". Also now the "cd" command navigates to the user's home directory if the "cd" command has no arguments or the first argument is an empty string

--- src/logic/builtins/pwd/pwd.c
+++ src/logic/builtins/pwd/pwd.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 18:04:41 by akovtune          #+#    #+#             */
-/*   Updated: 2025/03/19 12:39:01 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/03/22 18:29:28 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int	pwd(t_command *command)
 
 	if (!command || !command->environment)
 		return (FAILURE);
-	wd = get_env_variable(command->environment, "PWD");
+	wd = getcwd(NULL, 0);
 	if (!wd)
 		return (FAILURE);
 	write(STDOUT_FILENO, wd, ft_strlen(wd));
 	write(STDOUT_FILENO, "\n", 1);
+	free(wd);
 	return (SUCCESS);
 }
