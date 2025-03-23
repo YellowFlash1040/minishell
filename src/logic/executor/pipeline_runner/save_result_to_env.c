ccccc891c66156bf710afd5833c340eaf14e525f
akovtune <akovtune@student.codam.nl>
2025-03-23 16:55:09 +0100

    Changed logic inside the save_result_to_env() function

--- src/logic/executor/pipeline_runner/save_result_to_env.c
+++ src/logic/executor/pipeline_runner/save_result_to_env.c
@@ -6,22 +6,22 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 18:46:54 by akovtune          #+#    #+#             */
-/*   Updated: 2025/03/22 18:49:49 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/03/23 15:27:56 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipeline_runner.h"
 
-int	set_result_to_env(t_list *env, int pipeline_status_code)
+int	save_result_to_env(t_list *env, int pipeline_status_code)
 {
 	int			result;
-	t_string	variable_name;
+	t_string	string_status_code;
 
-	variable_name = ft_strdup("?");
-	if (!variable_name)
+	string_status_code = ft_itoa(pipeline_status_code);
+	if (!string_status_code)
 		return (FAILURE);
-	result = set_env_variable(env, variable_name, ft_itoa(pipeline_status_code), false);
+	result = set_env_variable(env, "?", string_status_code, false);
 	if (result != SUCCESS)
-		return (result);
+		return (free(string_status_code), result);
 	return (SUCCESS);
 }
