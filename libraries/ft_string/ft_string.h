58743b7d5fd689dafe631e0d7cb6262de42e2c15
ibenne <benne.ismo@gmail.com>
2025-04-07 12:34:01 +0200

    ft_memset and tests makefile restoration

--- libraries/ft_string/ft_string.h
+++ libraries/ft_string/ft_string.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_string.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ibenne <ibenne@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/08 17:35:18 by akovtune          #+#    #+#             */
-/*   Updated: 2025/04/02 15:11:39 by ibenne           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_string.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ibenne <ibenne@student.42.fr>                +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/08 17:35:18 by akovtune      #+#    #+#                 */
+/*   Updated: 2025/04/07 12:32:20 by ismo          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -213,6 +213,7 @@ char	**ft_split(char *s, char c);
  * @return A newly allocated string containing the extracted word.
  */
 char	*extract_word(char *s, int *i, char c);
+void	*ft_memset(void *s, int c, size_t n);
 
 /**
  * @brief Counts the number of words in a string separated by a delimiter.
--- src/logic/signals.c
+++ src/logic/signals.c
@@ -6,11 +6,12 @@
 /*   By: ismo <ismo@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/06 16:59:01 by ismo          #+#    #+#                 */
-/*   Updated: 2025/04/06 19:09:18 by ismo          ########   odam.nl         */
+/*   Updated: 2025/04/07 12:32:52 by ismo          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signals.h"
+#include "ft_string.h"
 #include <signal.h>
 #include <readline/readline.h>
 #include <unistd.h>
@@ -38,9 +39,8 @@ void	set_handlers(t_sigmode mode)
 	struct sigaction act_sigint;
 	struct sigaction act_sigquit;
 
-	memset(&act_sigint, 0, sizeof(act_sigint));
-	memset(&act_sigquit, 0, sizeof(act_sigquit));
-
+	ft_memset(&act_sigint, 0, sizeof(act_sigint));
+	ft_memset(&act_sigquit, 0, sizeof(act_sigquit));
 	if (mode == Interactive)
 	{
 		act_sigint.sa_handler = &interactive_sigint_handler;
