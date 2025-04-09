2c85c43a5541b269898f81424888de07facc733e
akovtune <akovtune@student.codam.nl>
2025-04-09 14:35:31 +0200

    Norminette fix for export.c and set.c

--- src/logic/builtins/set/set.c
+++ src/logic/builtins/set/set.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   set.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibenne <ibenne@student.42.fr>              +#+  +:+       +#+        */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 14:02:49 by akovtune          #+#    #+#             */
-/*   Updated: 2025/04/08 18:15:45 by ibenne           ###   ########.fr       */
+/*   Updated: 2025/04/09 12:33:48 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	set(t_command *command)
 	if (!command || !command->arguments || !command->environment)
 		return (FAILURE);
 	env = command->environment;
-	i = 1;
-	while (command->arguments[i])
+	i = 0;
+	while (command->arguments[++i])
 	{
 		tokens = create_token_list(command->arguments[i], true);
 		if (parse_variable(tokens, &name, &value) != SUCCESS)
@@ -37,7 +37,6 @@ int	set(t_command *command)
 				free(name), free(value), FAILURE);
 		free(name);
 		destroy_list(&tokens, free_token);
-		i++;
 	}
 	return (SUCCESS);
 }
