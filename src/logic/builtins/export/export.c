5aee5dc9ace8e89b65ea2f37ce257f388ede5490
akovtune <akovtune@student.codam.nl>
2025-04-11 14:26:00 +0200

    Made code inside "export" and "set" builtins more DRY

--- src/logic/builtins/export/export.c
+++ src/logic/builtins/export/export.c
@@ -1,42 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   export.c                                           :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: akovtune <akovtune@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2025/03/16 14:02:49 by akovtune      #+#    #+#                 */
-/*   Updated: 2025/04/09 19:18:25 by ismo          ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 14:02:49 by akovtune          #+#    #+#             */
+/*   Updated: 2025/04/11 14:12:25 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "export.h"
 
+int process_env_variable_manipulation_command(t_command *command,
+	bool is_exported);
+
 int	export(t_command *command)
 {
-	t_list			*env;
-	t_list			*tokens;
-	char			*name;
-	char			*value;
-	int				i;
-
-	if (!command || !command->arguments || !command->environment)
-		return (FAILURE);
-	env = command->environment;
-	i = 0;
-	while (command->arguments[++i])
-	{
-		tokens = create_token_list(command->arguments[i], true);
-		if (parse_variable(tokens, &name, &value) != SUCCESS)
-		{
-			print_error_message("export: parsing error\n");
-			return (destroy_list(&tokens, free_token), FAILURE);
-		}
-		if (set_env_variable(env, name, value, true) != SUCCESS)
-			return (destroy_list(&tokens, free_token),
-				free(name), free(value), FAILURE);
-		free(name);
-		destroy_list(&tokens, free_token);
-	}
-	return (SUCCESS);
+	int result;
+	
+	result = process_env_variable_manipulation_command(command, true);
+	return (result);
 }
--- src/logic/builtins/set/set.c
+++ src/logic/builtins/set/set.c
@@ -1,42 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   set.c                                              :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: akovtune <akovtune@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2025/03/16 14:02:49 by akovtune      #+#    #+#                 */
-/*   Updated: 2025/04/09 19:18:33 by ismo          ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   set.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 14:02:49 by akovtune          #+#    #+#             */
+/*   Updated: 2025/04/11 14:12:57 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "set.h"
 
+int process_env_variable_manipulation_command(t_command *command,
+	bool is_exported);
+
 int	set(t_command *command)
 {
-	t_list			*env;
-	t_list			*tokens;
-	char			*name;
-	char			*value;
-	int				i;
-
-	if (!command || !command->arguments || !command->environment)
-		return (FAILURE);
-	env = command->environment;
-	i = 0;
-	while (command->arguments[++i])
-	{
-		tokens = create_token_list(command->arguments[i], true);
-		if (parse_variable(tokens, &name, &value) != SUCCESS)
-		{
-			print_error_message("export: parsing error\n");
-			return (destroy_list(&tokens, free_token), FAILURE);
-		}
-		if (set_env_variable(env, name, value, false) != SUCCESS)
-			return (destroy_list(&tokens, free_token),
-				free(name), free(value), FAILURE);
-		free(name);
-		destroy_list(&tokens, free_token);
-	}
-	return (SUCCESS);
+	int result;
+	
+	result = process_env_variable_manipulation_command(command, false);
+	return (result);
 }
